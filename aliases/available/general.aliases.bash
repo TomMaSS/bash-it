8d9cc35a7720b400607ec536fd0a22a134700693
TomMaSS <aporubay@gmail.com>
2018-02-06 23:33:15 +0200

    Update general.aliases.bash

--- aliases/available/general.aliases.bash
+++ aliases/available/general.aliases.bash
@@ -11,7 +11,7 @@ fi
 # List directory contents
 alias sl=ls
 alias la='ls -AF'       # Compact view, show hidden
-alias ll='ls -hal'
+alias ll='ls -lah'
 alias l='ls -a'
 alias l1='ls -1'
 alias lf='ls -F'
