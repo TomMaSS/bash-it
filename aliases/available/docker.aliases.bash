ecf0067d6a45114634b16c07dace8ae102b84296
TomMaSS <aporubay@gmail.com>
2019-07-26 11:35:13 +0300

    alias dkrmi

--- aliases/available/docker.aliases.bash
+++ aliases/available/docker.aliases.bash
@@ -20,6 +20,7 @@ case $OSTYPE in
 		;;
 esac
 
+<<<<<<< HEAD
 if _bash-it-component-item-is-enabled plugin docker; then
 	# Function aliases from docker plugin:
 	alias dkrmlc='docker-remove-most-recent-container' # Delete most recent (i.e., last) Docker container
@@ -42,3 +43,11 @@ alias dkritc='docker run --rm -it -v $PWD:/cwd -w /cwd '
 alias dkip='docker image prune -a -f'
 alias dkvp='docker volume prune -f'
 alias dksp='docker system prune -a -f'
+=======
+alias dkrmall='docker-remove-stale-assets'  # Delete all untagged images and exited containers
+alias dkrmli='docker-remove-most-recent-image'  # Delete most recent (i.e., last) Docker image
+alias dkrmi='docker rmi &*'  # Delete images for supplied IDs or all if no IDs are passed as arguments
+alias dkideps='docker-image-dependencies'  # Output a graph of image dependencies using Graphiz
+alias dkre='docker-runtime-environment'  # List environmental variables of the supplied image ID
+alias dkelc='docker exec -it `dklcid` bash' # Enter last container (works with Docker 1.3 and above)
+>>>>>>> e43f1e0a (alias dkrmi)
